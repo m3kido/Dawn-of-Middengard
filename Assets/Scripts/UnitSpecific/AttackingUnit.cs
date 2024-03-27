7928ec99cfce6c301500157f5bb6629b7d384809
Khelifa Saadallah Oussama <oussakhema00@gmail.com>
2024-03-27 14:26:01 +0100

    multiweapons
    make attacking units with double weapons

--- Assets/Scripts/UnitSpecific/AttackingUnit.cs
+++ Assets/Scripts/UnitSpecific/AttackingUnit.cs
@@ -1,22 +1,46 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum EWeaponTypes
+{
+    Axe ,
+    Bow , 
+    Sword , 
+}
+public class Weapon
+{
+    [SerializeField] List<int> _damageList;
+    [SerializeField] int _ammo;
+    [SerializeField] EWeaponTypes _weaponType;
+
+    public List<int> DamageList { get => _damageList; set => _damageList = value; }
+    public int Ammo { get => _ammo; set => _ammo = value; }
+    public EWeaponTypes Type { get => _weaponType; set => _weaponType = value; }
+
+    // Add methods ki t3rf wsh homa
+}
+//public class WeaponDamage
+//{
+//    [SerializeField] List<int> _damageList; 
+//    public List<int> DamageList { get => _damageList; set => _damageList = value; }
+//}
 public class AttackingUnit : Unit
 {
-    // List of damages that this attacking unit can apply to other units 
-    [SerializeField] List<int> _damageList;
+    // List of damages that this attacking unit can apply to other units using each weapon 
+    [SerializeField] List<Weapon> _weapons; 
 
     [SerializeField] int _minRange;
     [SerializeField] int _maxRange;
+    [SerializeField] List<int> _ammo; //Ammo for first and second weapon 
 
-    public List<int> DamageList { get => _damageList; set => _damageList = value; }
+    public List<Weapon> Weapons { get => _weapons; set => _weapons = value; }
     public int MinRange { get => _minRange; set => _minRange = value; }
     public int MaxRange { get => _maxRange; set => _maxRange = value; }
 
-    public float CalculateDamage(Unit target, Unit attacker)
+    public float CalculateDamage(Unit target, Unit attacker , int weaponIndex )
     {
-
-        int baseDamage = _damageList[(int)target.Type];
+        
+        int baseDamage = _weapons[weaponIndex].DamageList[(int)target.Type];
         Player attackerPlayer = Gm.Players[attacker.Owner];
         Captain attackerCaptain = attackerPlayer.Captain;
         int celesteAttack = attackerPlayer.IsCelesteActive ? attackerCaptain.CelesteDefense : 0;
@@ -35,14 +59,14 @@ public class AttackingUnit : Unit
         return totalDamage;
     }
 
-    void ApplyDamage(Unit target, Unit attacker)
+    void ApplyDamage(Unit target, Unit attacker , int weaponIndex)
     {
-        var damage = CalculateDamage(target, attacker);
+        var damage = CalculateDamage(target, attacker ,  weaponIndex);
 
         target.Health -= (int)damage;
         if (target != null)
         {
-            damage = CalculateDamage(attacker, target);
+            damage = CalculateDamage(attacker, target ,  weaponIndex);
             attacker.Health -= (int)damage;
         }
 
@@ -50,7 +74,7 @@ public class AttackingUnit : Unit
     }
 
     // scans area for targets in an Intervall [ min range, max range[
-    List<Unit> ScanTargets(AttackingUnit attacker)
+    List<Unit> ScanTargets(AttackingUnit attacker , int weaponIndex)
     {
         var attackerPos = Mm.Map.WorldToCell(attacker.transform.position);
         List<Unit> targets = new();
@@ -61,7 +85,7 @@ public class AttackingUnit : Unit
 
             bool IsInRange = (L1Distance(attackerPos, potentialTargetPos) >= attacker.MinRange) && (L1Distance(attackerPos, potentialTargetPos) < attacker.MaxRange);
             bool IsEnemy = attacker.Owner != unit.Owner;
-            bool IsDamageable = attacker.DamageList[(int)unit.Type] != 0;
+            bool IsDamageable = attacker._weapons[weaponIndex].DamageList[(int)unit.Type] != 0;
 
             if (IsInRange && IsEnemy && IsDamageable)
             {
